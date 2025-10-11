b955a241cf8257e71159d2955add66a098031f92
Charlie Must <must.2@wright.edu>
2025-10-10 23:40:42 -0400

    Brain dump corrected
    Refactored SequenceNode class to include member variables and constructors within the class definition.

--- SequenceNode.cpp
+++ SequenceNode.cpp
@@ -7,18 +7,26 @@
 // Implements the SequenceNode class used in the doubly-linked Sequence structure.
 // Each node stores a string item and pointers to adjacent nodes.
 // This file defines the constructors for node initialization.
+#include <string>
 
-#include "SequenceNode.h"
-
-/**
- * @brief Default constructor.
- * Initializes item to empty string and pointers to nullptr.
- */
-SequenceNode::SequenceNode() : item(""), next(nullptr), prev(nullptr) {}
-
-/**
- * @brief Parameterized constructor.
- * @param item The string to store in the node.
- * Initializes pointers to nullptr.
- */
-SequenceNode::SequenceNode(const std::string& item) : item(item), next(nullptr), prev(nullptr) {}
+class SequenceNode {
+    SequenceNode *next;
+    SequenceNode *prev;
+    std::string item;
+    /**
+     * @brief Default constructor.
+     * Initializes item to empty string and pointers to nullptr.
+     */
+    SequenceNode() : next(nullptr),
+                     prev(nullptr) {
+    }
+    /**
+     * @brief Parameterized constructor.
+     * @param item The string to store in the node.
+     * Initializes pointers to nullptr.
+     */
+    SequenceNode(const std::string &item) : next(nullptr),
+                                     prev(nullptr),
+                                     item(item) {
+    }
+};
