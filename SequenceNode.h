c4594d6971bf8c2a0337787c1eda9184cee7537d
Charlie Must <must.2@wright.edu>
2025-10-10 23:14:32 -0400

    Refactor SequenceNode class with improved constructors
    Updated constructors and added documentation for clarity.

--- SequenceNode.h
+++ SequenceNode.h
@@ -1,15 +1,29 @@
 #ifndef PROJECT3_SEQUENCENODE_H
 #define PROJECT3_SEQUENCENODE_H
+
 #include <string>
 
+/**
+ * @brief A node in the doubly-linked Sequence.
+ * Stores a string item and pointers to adjacent nodes.
+ */
 class SequenceNode {
 public:
-    std::string item;
-    SequenceNode* next;
-    SequenceNode* prev;
+    std::string item;       ///< The data stored in this node.
+    SequenceNode* next;     ///< Pointer to the next node (nullptr if tail).
+    SequenceNode* prev;     ///< Pointer to the previous node (nullptr if head).
 
-    SequenceNode(std::string &item) : item(item), next(nullptr), prev(nullptr) {}
+    /**
+     * @brief Default constructor.
+     * Initializes pointers to nullptr.
+     */
+    SequenceNode() : item(""), next(nullptr), prev(nullptr) {}
 
-    SequenceNode() : next(nullptr), prev(nullptr){};
+    /**
+     * @brief Parameterized constructor.
+     * @param item The string to store in the node.
+     */
+    SequenceNode(const std::string& item) : item(item), next(nullptr), prev(nullptr) {}
 };
+
 #endif //PROJECT3_SEQUENCENODE_H
